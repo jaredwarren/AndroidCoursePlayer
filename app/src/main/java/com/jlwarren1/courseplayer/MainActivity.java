96ad28b8524b1878b33f7292b6ac3407768446aa
Jared Warren <jaredwarren@rapidintake.com>
2016-01-18 16:41:53 -0700

    working on menu

--- app/src/main/java/com/jlwarren1/courseplayer/MainActivity.java
+++ app/src/main/java/com/jlwarren1/courseplayer/MainActivity.java
@@ -2,31 +2,38 @@ package com.jlwarren1.courseplayer;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.SubMenu;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout mDrawerLayout;
+    ExpandableListAdapter mMenuAdapter;
+    ExpandableListView expandableList;
+    List<ExpandedMenuModel> listDataHeader;
+    HashMap<ExpandedMenuModel, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
+        // fab
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,164 +43,101 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Setup toolbar action button
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
         toggle.syncState();
 
-        //NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        //navigationView.setNavigationItemSelectedListener(this);
 
-        setGroupData();
-        setChildGroupData();
 
-        initDrawer();
+        // Setup nav view
+        expandableList = (ExpandableListView) findViewById(R.id.expanded_menu);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
-        //navigationView.se
-
-        //var navView = (NavigationView) findViewById(R.id.drawMenu);
-        /*Menu m = navigationView.getMenu();
-        SubMenu topChannelMenu = m.addSubMenu("Top Channels");
-        SubMenu s1 = topChannelMenu.addSubMenu("sub1");
-        topChannelMenu.add("Foo");
-        s1.add("Bar");
-        s1.add("Baz");*/
-    }
-
-    //private DrawerLayout drawer;
-    private ExpandableListView drawerList;
-    private void initDrawer() {
-        //drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        drawerList = (ExpandableListView) findViewById(R.id.left_drawer);
-
-        drawerList.setAdapter(new NewAdapter(this, groupItem, childItem));
-
-        //drawerList.setOnChildClickListener(this);
-
-        // actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer,
-        // R.drawable.ic_drawer, R.string.open_drawer,
-        // R.string.close_drawer) {
-        // public void onDrawerClosed(View view) {
-        // getActionBar().setSubtitle("open");
-        // }
-        //
-        // /** Called when a drawer has settled in a completely open state. */
-        // public void onDrawerOpened(View drawerView) {
-        // getActionBar().setSubtitle("close");
-        // }
-        //
-        // };
-        //
-        // drawer.setDrawerListener(actionBarDrawerToggle);
-
-    }
-
-    public void setGroupData() {
-        groupItem.add("Technology");
-        groupItem.add("Mobile");
-        groupItem.add("Manufacturer");
-        groupItem.add("Extras");
-    }
-
-    ArrayList<String> groupItem = new ArrayList<String>();
-    ArrayList<Object> childItem = new ArrayList<Object>();
-
-    public void setChildGroupData() {
-        /**
-         * Add Data For TecthNology
-         */
-        ArrayList<String> child = new ArrayList<String>();
-        child.add("Java");
-        child.add("Drupal");
-        child.add(".Net Framework");
-        child.add("PHP");
-        childItem.add(child);
-
-        /**
-         * Add Data For Mobile
-         */
-        child = new ArrayList<String>();
-        child.add("Android");
-        child.add("Window Mobile");
-        child.add("iPHone");
-        child.add("Blackberry");
-        childItem.add(child);
-        /**
-         * Add Data For Manufacture
-         */
-        child = new ArrayList<String>();
-        child.add("HTC");
-        child.add("Apple");
-        child.add("Samsung");
-        child.add("Nokia");
-        childItem.add(child);
-        /**
-         * Add Data For Extras
-         */
-        child = new ArrayList<String>();
-        child.add("Contact Us");
-        child.add("About Us");
-        child.add("Location");
-        child.add("Root Cause");
-        childItem.add(child);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+        if (navigationView != null) {
+            //setupDrawerContent(navigationView);
         }
+
+        prepareListData();
+        mMenuAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild, expandableList);
+
+        // setting list adapter
+        expandableList.setAdapter(mMenuAdapter);
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        ExpandedMenuModel item1 = new ExpandedMenuModel();
+        item1.setIconName("heading1");
+        item1.setIconImg(R.drawable.ic_menu_gallery);
+        // Adding data header
+        listDataHeader.add(item1);
+
+        ExpandedMenuModel item2 = new ExpandedMenuModel();
+        item2.setIconName("heading2");
+        item2.setIconImg(R.drawable.ic_menu_camera);
+        listDataHeader.add(item2);
+
+        ExpandedMenuModel item3 = new ExpandedMenuModel();
+        item3.setIconName("heading3");
+        item3.setIconImg(R.drawable.ic_menu_send);
+        listDataHeader.add(item3);
+
+        // Adding child data
+        List<String> heading1 = new ArrayList<>();
+        heading1.add("Submenu of item 1");
+
+        List<String> heading2 = new ArrayList<>();
+        heading2.add("Submenu of item 2");
+        heading2.add("Submenu of item 2");
+        heading2.add("Submenu of item 2");
+
+        listDataChild.put(listDataHeader.get(0), heading1);// Header, Child data
+        listDataChild.put(listDataHeader.get(1), heading2);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        Log.i("...", "on tap:"+item.toString());
+        item.setChecked(true);
+        mDrawerLayout.closeDrawers();
         return true;
     }
 }
